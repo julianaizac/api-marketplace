c54e3a22c6f1d26aaaf3378abc96c29b02144357
julianaizac <dev.julianaizac@outlook.com>
2023-05-10 19:44:31 -0300

    first commit

---
+++ src/main/java/com/facens/apimarketplace/ApiMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.facens.apimarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facens/apimarketplace/ApiMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facens.apimarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
